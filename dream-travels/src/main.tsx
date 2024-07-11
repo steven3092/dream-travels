6ddabb11020f32b754981e0fa84b11866f2d97b8
steven3092 <steven.fauterre@groupe-esigelec.org>
2024-07-11 19:40:40 +0000

    set react-query config

--- dream-travels/src/main.tsx
+++ dream-travels/src/main.tsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import './index.css'
+ 
+const queryClient = new QueryClient();
+ 
+const rootElement = document.getElementById('root')!;
+ 
+if (!rootElement.innerHTML) {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
+  );
+}
