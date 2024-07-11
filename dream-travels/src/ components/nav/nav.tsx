ad8fafad8cb8b57a0cfa5c53cb11f7064164c949
steven3092 <steven.fauterre@groupe-esigelec.org>
2024-07-11 19:40:25 +0000

    create nav

---
+++ dream-travels/src/ components/nav/nav.tsx
@@ -0,0 +1,14 @@
+import { Button } from "../button/button"
+import dreamTripIcon from "../../assets/dream-trips-icon.svg"
+
+
+
+export const Nav = () => {
+
+    return(
+        <nav className="dream-trips-nav">
+            <img src={dreamTripIcon} alt="dot logo" />
+            <Button type="button" name='test' backgroundColor='white' color='black'/>
+        </nav>
+    )
+}
