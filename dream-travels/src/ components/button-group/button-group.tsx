db6d5edc8983f596a6458978fb72374a12a70fd1
steven fauterre <steven.fauterre@wefox.com>
2024-07-13 18:15:43 +0200

    create button group

---
+++ dream-travels/src/ components/button-group/button-group.tsx
@@ -0,0 +1,39 @@
+import { useGetTrips } from "../../hooks/use-get-trips";
+import { useTrips } from "../../hooks/use-trips/use-trips";
+import { TripsDTO } from "../../interfaces/trips.dto.interface";
+import "./button-group.scss";
+
+export const ButtonGroup = ({
+  leftButton,
+  middleButton,
+  rightButton,
+  handleOnClickAllTrips,
+  handleOnClickUpcomingTrips,
+  handleOnClickCompletedTrips,
+}: {
+  leftButton: string;
+  middleButton: string;
+  rightButton: string;
+  handleOnClickAllTrips: (trips: TripsDTO[]) => void;
+  handleOnClickUpcomingTrips: () => void;
+  handleOnClickCompletedTrips: (status: string) => void;
+}) => {
+  const { state } = useTrips();
+
+  return (
+    <div className="button-group">
+      <button className="btn" onClick={() => handleOnClickAllTrips(state)}>
+        {leftButton}
+      </button>
+      <button className="btn" onClick={handleOnClickUpcomingTrips}>
+        {middleButton}
+      </button>
+      <button
+        className="btn"
+        onClick={() => handleOnClickCompletedTrips("completed")}
+      >
+        {rightButton}
+      </button>
+    </div>
+  );
+};
