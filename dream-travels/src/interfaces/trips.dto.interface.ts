be4b0863c4d98e38b2d5bd39184590aa1156cce2
steven fauterre <steven.fauterre@wefox.com>
2024-07-13 18:10:58 +0200

    create interface

---
+++ dream-travels/src/interfaces/trips.dto.interface.ts
@@ -0,0 +1,14 @@
+export interface TripsDTO {
+  readonly id: number;
+  readonly itinerary: ItineraryDTO[];
+  readonly description: string;
+  readonly photo_url: string;
+  readonly status: string;
+  readonly title: string;
+}
+
+export interface ItineraryDTO {
+  readonly day: string;
+  readonly description: string;
+  readonly location: string;
+}
