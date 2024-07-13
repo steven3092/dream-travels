38a3c4a9b40d6ea27fed8e3a760440e4f3658cc9
steven fauterre <steven.fauterre@wefox.com>
2024-07-13 18:13:52 +0200

    create trip card

---
+++ dream-travels/src/ components/trip-card/trip-card.tsx
@@ -0,0 +1,93 @@
+import { useModal } from "../modal/hooks/use-modal";
+import { Modal } from "../modal/modal";
+import { TripModalDetail } from "../trip-modal-detail/trip-modal-detail";
+import { ItineraryDTO, TripsDTO } from "../../interfaces/trips.dto.interface";
+import { TripModalEdit } from "../trip-modal-edit/trip-modal-edit";
+import "./trip-card.scss";
+
+export const TripCard = ({
+  imageUrl,
+  title,
+  description,
+  itinerary,
+  id,
+  handleOnTripChangeStatus,
+  handleOnTripEditSubmit,
+  handleOnDeleteTrip,
+}: {
+  imageUrl: string;
+  title: string;
+  description: string;
+  itinerary: ItineraryDTO[];
+  id: number;
+  handleOnTripChangeStatus: (trip: TripsDTO) => void;
+  handleOnTripEditSubmit: (trip: TripsDTO) => void;
+  handleOnDeleteTrip: (id: number) => void;
+}) => {
+  const { isOpen: isOpenModalDetail, toggleStatus: toggleStatusModalDetail } =
+    useModal();
+  const { isOpen: isOpenModalEdit, toggleStatus: toggleStatusModalEdit } =
+    useModal();
+
+  const handleOnCloseModalEdit = () => {
+    toggleStatusModalEdit();
+  };
+
+  return (
+    <>
+      <div className="dream-trip-card">
+        <img src={imageUrl} alt="no image" className="dream-trip-card-image" />
+        <div className="dream-trip-card-text-content">
+          <h2>{title}</h2>
+          <div>{description}</div>
+          <div className="dream-trip-card-buttons">
+            <button
+              type="button"
+              className="dream-trip-card-button-action"
+              onClick={toggleStatusModalDetail}
+            >
+              See trips detail
+            </button>
+            <div className="dream-trip-card-buttons-right">
+              <button
+                type="button"
+                className="dream-trip-card-button-action"
+                onClick={toggleStatusModalEdit}
+              >
+                Edit
+              </button>
+              <button
+                type="button"
+                className="dream-trip-card-button-action-delete"
+                onClick={() => handleOnDeleteTrip(id)}
+              >
+                Delete
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Modal isOpen={isOpenModalDetail} toggleStatus={toggleStatusModalDetail}>
+        <TripModalDetail
+          imageUrl={imageUrl}
+          title={title}
+          description={description}
+          itinerary={itinerary}
+          id={id}
+          handleOnTripChangeStatus={handleOnTripChangeStatus}
+        />
+      </Modal>
+      <Modal isOpen={isOpenModalEdit} toggleStatus={toggleStatusModalEdit}>
+        <TripModalEdit
+          imageUrl={imageUrl}
+          title={title}
+          description={description}
+          itinerary={itinerary}
+          id={id}
+          handleOnTripEditSubmit={handleOnTripEditSubmit}
+          handleOnCloseModalEdit={handleOnCloseModalEdit}
+        />
+      </Modal>
+    </>
+  );
+};
