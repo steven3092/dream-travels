195a7dbb09907159803d917de7134123b5df906d
steven fauterre <steven.fauterre@wefox.com>
2024-07-13 18:15:51 +0200

    create button

--- dream-travels/src/ components/button/button.tsx
+++ dream-travels/src/ components/button/button.tsx
@@ -5,17 +5,20 @@ export const Button = ({
   type,
   backgroundColor,
   color,
+  onClick,
 }: {
   name: string;
   type: "submit" | "reset" | "button" | undefined;
   backgroundColor: string;
   color: string;
+  onClick?: () => void;
 }) => {
   return (
     <button
       style={{ backgroundColor: backgroundColor, color: color }}
       type={type}
       className="dream-trips-button"
+      onClick={onClick}
     >
       {name}
     </button>
