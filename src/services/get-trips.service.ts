60362bef2905e43dd5007c1985f6d35a86a7d82c
steven fauterre <steven.fauterre@wefox.com>
2024-07-14 16:57:36 +0200

    rename file

---
+++ src/services/get-trips.service.ts
@@ -0,0 +1,10 @@
+export async function fetchTrips() {
+  const response = await fetch(
+    "https://my-json-server.typicode.com/mariosanz92/dream-travels-data/travels"
+  );
+  const data = await response.json();
+
+  const trips = data;
+
+  return trips;
+}
--- src/services/get-trips.ts
+++
@@ -1,9 +0,0 @@
-
-export async function fetchTrips() {
-  const response = await fetch('https://my-json-server.typicode.com/mariosanz92/dream-travels-data/travels');
-  const data = await response.json();
-  
-  const trips = data;
-
-  return trips;
-}
